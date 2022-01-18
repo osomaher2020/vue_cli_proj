eccf6adea55893cf397273c2a20b9bd23b029c42
osomaher2020 <osomaher1@gmail.com>
2022-01-18 03:31:04 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{title}}</h1>
+  <input type="text" ref="student_name" v-model.lazy="name">
+  <button @click="handleClick">Click Me!</button>
+  <hr>
+  <MyComponent header="my props Header" :student_name="name" />
+</template>
+
+<script>
+import MyComponent from './components/MyComponent'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  },
+  data() {
+    return {
+      title: 'My Vue App :)',
+      name: 'osama'
+    }
+  },
+  methods: {
+    handleClick(){
+      this.$refs.student_name.value = "clicked"
+      this.$refs.student_name.classList.add('active')
+      this.$refs.student_name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>{{ header }}</h3>
+    <p>{{student_name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['header', 'student_name'],
+  data(){
+    return {
+      msg: 'Hello from myComponent'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
